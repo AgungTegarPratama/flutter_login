a7db346435ca05305211c82efb4869e260eab14e
AinunNurlita <ainunnurlita2@gmail.com>
2026-05-11 22:27:05 +0700

    ainun update

---
+++ lib/src/user_repository.dart
@@ -0,0 +1,18 @@
+import 'package:ppb2_flutter_login/src/models/models.dart';
+import 'package:uuid/uuid.dart';
+
+
+class UserRepository {
+  User? _user;
+
+  Future<User?> getUser() async {
+    if (_user != null) return _user;
+
+    return Future.delayed(
+      const Duration(milliseconds: 300),
+      () => _user = User(
+        const Uuid().v4(),
+      ),
+    );
+  }
+}
